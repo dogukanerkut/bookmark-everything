696a0c9512828fc19d5cd617207bb3255da90ac5
dogukanerkut <dogukanerkut@gmail.com>
2018-12-02 23:59:41 +0300

    long forgotten EntryDataGUIDComparer add

---
+++ BookmarkEverything/Assets/BookmarkEverything/Editor/EntryDataGUIDComparer.cs
@@ -0,0 +1,17 @@
+
+using System.Collections.Generic;
+namespace BookmarkEverything
+{
+    public class EntryDataGUIDComparer : EqualityComparer<BookmarkEverythingEditor.EntryData>
+    {
+        public override bool Equals(BookmarkEverythingEditor.EntryData x, BookmarkEverythingEditor.EntryData y)
+        {
+            return x.GUID == y.GUID;
+        }
+
+        public override int GetHashCode(BookmarkEverythingEditor.EntryData obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
