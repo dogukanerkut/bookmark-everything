50a1236490693f45468d2662c0a111d33e9c98d3
dogukanerkut <dogukanerkut@gmail.com>
2018-12-07 00:33:43 +0300

    Removed license texts and some "funny" comments

--- BookmarkEverything/Assets/BookmarkEverything/Editor/BookmarkEverythingEditor.cs
+++ BookmarkEverything/Assets/BookmarkEverything/Editor/BookmarkEverythingEditor.cs
@@ -1,43 +1,12 @@
-﻿/*
-MIT License
-
-Copyright (c) 2018 Doğukan Erkut
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
--------------------------------------------------------------------------------
-
-Repository: https://github.com/dogukanerkut/bookmark-everything
-Contact: dogukanerkut@gmail.com
- */
-using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 using System.Text;
-using System.IO;
 
 namespace BookmarkEverything
 {
-    public class BookmarkEverythingEditor : EditorWindow
+	public class BookmarkEverythingEditor : EditorWindow
     {
         private SaveData _currentSettings = new SaveData();
 
@@ -295,7 +264,7 @@ namespace BookmarkEverything
                         {
                             if(!AssetDatabase.Contains(draggedObject))
                             {
-                                EditorUtility.DisplayDialog("Bookmark Everything", "Objects from hierarchy is not supported for now. Would you like me to add that? Please e-mail me at dogukanerkut@gmail.com.\n Or you know, you can hack through the codes and add it yourself, but I do not recommend that. Anyway, this was a way long message then it's supposed to, I guess I like talking people through annoying display dialogs of Unity(really hate them for some reason).", "Okay, chill dude.");
+                                EditorUtility.DisplayDialog("Bookmark Everything", "Objects from hierarchy is not supported for now. Would you like me to add that? Please e-mail me at dogukanerkut@gmail.com.", "Okay");
                                 return;
                             }
                             EntryData entryData = new EntryData(draggedObject);
